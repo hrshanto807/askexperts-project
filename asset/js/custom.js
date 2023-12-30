f7b10b6d3c9712614adf7517a70482b4ec234818
hrsanto <shantosss4907@gmail.com>
2023-12-30 13:47:16 +0600

    service-single html done

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,11 +1,11 @@
 (function ($) {
     $('#main_menu ul ul').parent('li').children('a').append('<i class="fa-solid fa-chevron-right"></i>')
 
-$('.header-menu-area > a').click(function(){
-    $('#main_menu').slideToggle();
+    $('.header-menu-area > a').click(function () {
+        $('#main_menu').slideToggle();
 
 
-})
+    })
 
     $('.lef-value').circleProgress({
         value: 0.8,
@@ -29,47 +29,72 @@ $('.header-menu-area > a').click(function(){
         $('.aep-brand-logo').owlCarousel({
             loop: true,
             nav: false,
-            dots: false,           
-            responsive:{
-                0:{
-                    items:2
+            dots: false,
+            responsive: {
+                0: {
+                    items: 2
                 },
-                480:{
-                    items:3
+                480: {
+                    items: 3
                 },
-                768:{
-                    items:3
+                768: {
+                    items: 3
                 },
-                1000:{
-                    items:5
+                1000: {
+                    items: 5
                 },
             },
             autoplayHoverPause: true,
-            
+
         }),
         // owl-carousel for testimonial
         $('.aep-home-testimonials').owlCarousel({
             loop: true,
             nav: true,
             dots: false,
-            responsive:{
-                0:{
-                    items:1
+            responsive: {
+                0: {
+                    items: 1
                 },
-                480:{
-                    items:1
+                480: {
+                    items: 1
                 },
-                768:{
-                    items:1
+                768: {
+                    items: 1
                 },
-                1000:{
-                    items:1
+                1000: {
+                    items: 1
                 },
             },
             autoplay: true,
             autoplayTimeout: 10000,
             autoplayHoverPause: true,
             navText: ['<i class="fa-solid fa-arrow-left-long"></i><p>Prev</p>', '<p>Next</p><i class="fa-solid fa-arrow-right-long"></i>'],
+
+        }),
+        // owl-carousel for service pages
+        $('.aep-service-finance-area').owlCarousel({
+            loop: true,
+            nav: true,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                1000: {
+                    items: 3
+                },
+            },
+            autoplay: true,
+            autoplayTimeout: 10000,
+            autoplayHoverPause: true,
+            navText: ['<i class="fa-solid fa-arrow-left-long"></i>', '<i class="fa-solid fa-arrow-right-long"></i>'],
 
         }),
 
@@ -80,25 +105,34 @@ $('.header-menu-area > a').click(function(){
 
         }),
         // scroll top
-        jQuery(window).scroll(function () { 
-            
-        });
-        $('.scrollTop').click(function(){
-            $('html').animate({'scrollTop':'0'},2000);
-            return false;
+        jQuery(window).scroll(function () {
 
-        })
-        // magnific poppup video
-
-        $(document).ready(function() {
-            $('.video-play').magnificPopup({
-                disableOn: 700,
-                type: 'iframe',
-                mainClass: 'mfp-fade',
-                removalDelay: 160,
-                preloader: false,        
-                fixedContentPos: false
-            });
         });
+    $('.scrollTop').click(function () {
+        $('html').animate({ 'scrollTop': '0' }, 2000);
+        return false;
+
+    })
+    // magnific poppup video
+
+    $(document).ready(function () {
+        $('.video-play').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+    });
+    // faqs
+    $('.single-accordian:nth-child(2)').children('.single-accordian-content').slideDown().prev('.single-accordian-tittle').addClass('active-accordian').removeClass('minus');
+
+    $('.single-accordian-tittle').click(function () {
+        $(this).toggleClass('active-accordian').toggleClass('minus').next().slideToggle().parent('.single-accordian').siblings('.single-accordian').children('.single-accordian-content').hide();
+
+        $(this).parent('.single-accordian').siblings('.single-accordian').children('.single-accordian-tittle').removeClass('active-accordian').addClass('minus');
+       
+    });
 
 })(jQuery);
